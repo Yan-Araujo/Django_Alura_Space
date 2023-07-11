b9263b70766702483fe45893b9f43bebb6a772c4
Yan Araujo <yanaraujo@air-de-yan.lan>
2023-07-11 16:05:18 -0300

    Criação do app 'usuarios'

--- setup/settings.py
+++ setup/settings.py
@@ -42,7 +42,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'galeria',
+    'galeria.apps.GaleriaConfig',
+    'usuarios.apps.UsuariosConfig',
 ]
 
 MIDDLEWARE = [
